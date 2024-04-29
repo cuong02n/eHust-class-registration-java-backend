1e9e33f9cb94678f620cf42dd6393d55e9dffd0c
cuong02n <cuong02n@gmail.com>
2024-04-29 16:11:00 +0700

    Add Nullable annotation for BaseEntity

--- src/main/java/vn/edu/hust/ehustclassregistrationjavabackend/model/entity/BaseEntity.java
+++ src/main/java/vn/edu/hust/ehustclassregistrationjavabackend/model/entity/BaseEntity.java
@@ -1,7 +1,7 @@
 package vn.edu.hust.ehustclassregistrationjavabackend.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.gson.annotations.Expose;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -14,30 +14,36 @@ import java.sql.Timestamp;
 public abstract class BaseEntity {
     @Column(name = "createdBy")
     @Expose
+    @Nullable
     String createdById;
 
     @JoinColumn(name = "createdBy", updatable = false, insertable = false)
     @ManyToOne(fetch = FetchType.LAZY)
-    @Expose(serialize = false )
+    @Expose(serialize = false)
+    @Nullable
     User createdBy;
 
     @Column(name = "updatedBy")
     @Expose
+    @Nullable
     String updatedById;
 
     @JoinColumn(name = "updatedBy", updatable = false, insertable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     @Expose(serialize = false)
+    @Nullable
     User updatedBy;
 
     @CreationTimestamp
     @Column(name = "createdTime", updatable = false)
     @Expose
+    @Nullable
     Timestamp createdTime;
 
     @UpdateTimestamp
     @Column(name = "updatedTime")
     @Expose
+    @Nullable
     Timestamp updatedTime;
 
     public void update(String userId) {
